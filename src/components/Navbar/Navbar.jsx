ff8c06db6f72b8d60c56abf369eeb78d5eddeddd
Luvaha <luvaha456@gmail.com>
2026-04-19 22:07:02 +0300

    Responsive hamburger menu design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./Navbar.scss"
 
 function Navbar() {
+    const [open,setOpen] = useState(false)
     return (
         <nav>
             <div className="left">
@@ -17,6 +19,17 @@ function Navbar() {
             <div className="right">
                 <a href="/">Sign In</a>
                 <a href="/" className="register">Sign Up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="" onClick={()=>setOpen(prev=>!prev)}/>
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign In</a>
+                    <a href="/">Sign Up</a>
+                </div>
             </div>
         </nav>
     )
