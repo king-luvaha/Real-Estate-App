9d2ccab3ebf0fdef5e2f5e9beb1ad385425ba356
Luvaha <luvaha456@gmail.com>
2026-04-28 19:28:02 +0300

    Responsive Profile Page

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,8 +1,39 @@
+import List from '../../components/list/List'
 import './profilePage.scss'
 
 function ProfilePage(){
     return (
-        <div className='profilePage'>ProfilePage</div>
+        <div className='profilePage'>
+            <div className='details'>
+                <div className='wrapper'>
+                    <div className='title'>
+                        <h1>User Information</h1>
+                        <button>Update Profile</button>
+                    </div>
+
+                    <div className="info">
+                        <span>Avatar: <img src='https://images.pexels.com/photos/19651140/pexels-photo-19651140.jpeg' alt=''/></span>
+                        <span>Username: <b>John Doe</b></span>
+                        <span>Email: <b>doe@gmail.com</b></span>
+                    </div>
+
+                    <div className='title'>
+                        <h1>My List</h1>
+                        <button>Create New Post</button>
+                    </div>
+                    <List />
+                    <div className='title'>
+                        <h1>Saved List</h1>
+                    </div>
+                    <List />
+
+                </div>
+            </div>
+
+            <div className='chatContainer'>
+                <div className='wrapper'></div>
+            </div>
+        </div>
     )
 }
 
